cbc073b17ff19b8980e2f5b0e513bbe6ed82f694
lufebadeca <lufebadeca@gmail.com>
2023-05-29 20:33:01 -0500

    files and src folder added

---
+++ src/challengePromise.js
@@ -0,0 +1,38 @@
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;  //must install npm xmlhttprequest
+const API = 'https://api.escuelajs.co/api/v1';  //products endpoint URL
+
+function fetchMyData(urlApi, callback){ //function to prepare and control the request flow
+    let xhttp =  new XMLHttpRequest();  //allows control on the flow od the call, instead of fetch
+
+    xhttp.open('GET', urlApi, true);    //uses object and opens a connection to API, true to enable
+    xhttp.onreadystatechange = function( event ) {  //event to lsiten request behavior
+        if (xhttp.readyState === 4){    //4 for completed, === to validate value and type
+            if(xhttp.status === 200){   //returns status of the request RESPONSE. 200 for OK
+                callback(null, JSON.parse(xhttp.responseText)); //transforms data into JSON object
+            }
+            else{   //if response is not 200 means error
+                const error =  new Error('Error '+ urlApi); //creates an error object?
+                return callback(error, null);   //returns nothing but the error 
+            }
+        }
+    }
+    xhttp.send();   //sends the pettition
+};
+
+//the following portion will use the custom function several times to retrieve several example data from the API:
+fetchMyData(`${API}/products`, function(error1,data1){    //gets all products and stores it into data1
+    if(error1)  return console.error(error1);   //if failed request, show error
+    console.log(data1[0]);  //from all products obtained, show the first
+
+    fetchMyData(`${API}/products/${data1[0].id}`, function(error2,data2){ //gets the first product's id and stores into data2 
+        if(error2) return console.error(error2);
+        console.log(data2.title);   //show the title for the given id (of the first product)
+
+        fetchMyData(`${API}/categories/${data2?.category?.id}`, function(error3,data3){   //gets the category id of first product, given its id
+            if(error3) return console.error(error3);
+            console.log(data3.name);
+        });
+    });
+});
+//the fetch my data calls are nested since we are using data from the previous calls later on new calls while still available in runtime.
+//This is called callback hell 
